896875c25581cd5541dbd5317389f6696fe5881e
SunnySharma04 <sunnysharma112004@gmail.com>
2025-08-03 20:47:07 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,141 +3,115 @@ const UserModel = require("../model/UserModel");
 const sendJwtToekn = require("../appUtills/jwtToken");
 const cloudinary = require("cloudinary");
 const ErrorHandler = require("../appUtills/error");
-const userModel = require("../model/UserModel");
- 
- 
 
-//>>>>>>get all search user expect this user <<<<<<<<<
-//@description     Get or Search all users
-//@route           GET /api/user?search=
-//@access          Public
-
-
-exports.allSearchUser = asyncWrapper(async (req, res) => {
-  // Get the search keyword from the query parameter, if present
+// @desc    Search users (excluding current user)
+// @route   GET /api/user?search=
+// @access  Private
+exports.allSearchUser = asyncWrapper(async (req, res, next) => {
   const keyword = req.query.search
     ? {
         $or: [
-          {
-            name: { $regex: req.query.search, $options: "i" }, // search by name (case-insensitive)
-          },
-          {
-            email: { $regex: req.query.search, $options: "i" }, // search by email (case-insensitive)
-          },
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
         ],
       }
     : {};
 
-  // Find all users that match the search criteria, excluding[$ne: not equals] the currently logged-in user (req.user)
   const users = await UserModel.find(keyword).find({
     _id: { $ne: req.user._id },
   });
 
-  // Send the list of matching users in the response
-  res.json(users); // ✅ Sends actual JSON
-
-
+  res.status(200).json(users);
 });
 
-
-// >>>>> Create User Api <<<<<<<<< 
-//@description     Register new user
-//@route           POST /api/user/
-//@access          Public
+// @desc    Register new user
+// @route   POST /api/user
+// @access  Public
 exports.registerUser = asyncWrapper(async (req, res, next) => {
-  try {
-    console.log("Register Request Body:", req.body);
+  const { name, email, password, pic } = req.body;
 
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.pic, {
+  if (!name || !email || !password) {
+    return next(new ErrorHandler("Please enter all required fields", 400));
+  }
+
+  const userExists = await UserModel.findOne({ email });
+
+  if (userExists) {
+    return next(new ErrorHandler("User already exists", 400));
+  }
+
+  let picUrl = pic;
+  if (pic) {
+    const myCloud = await cloudinary.v2.uploader.upload(pic, {
       folder: "profile",
       width: 150,
       crop: "scale",
     });
-
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-      return next(new ErrorHandler("Please Enter all the Fields", 400));
-    }
-
-    const userExits = await UserModel.findOne({ email });
-
-    if (userExits) {
-      return next(new ErrorHandler("User already exists", 400));
-    }
-
-    const user = await UserModel.create({
-      name,
-      password,
-      email,
-      pic: myCloud.secure_url,
-    });
-
-    if (!user) {
-      return next(new ErrorHandler("User creation failed", 500));
-    }
-
-    sendJwtToekn(user, 201, res);
-
-  } catch (err) {
-    console.error("Register error:", err); // ✅ show full error in terminal
-    next(err); // send to middleware
+    picUrl = myCloud.secure_url;
   }
+
+  const user = await UserModel.create({
+    name,
+    email,
+    password,
+    pic: picUrl,
+  });
+
+  if (!user) {
+    return next(new ErrorHandler("User creation failed", 500));
+  }
+
+  sendJwtToekn(user, 201, res);
 });
 
-
-// >>>>>> login user <<<<<<<<
-//@description     Auth the user
-//@route           POST /api/users/login
-//@access          Public
+// @desc    Login user
+// @route   POST /api/user/login
+// @access  Public
 exports.loginController = asyncWrapper(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ErrorHandler("Please Enter Email & Password", 400));
+    return next(new ErrorHandler("Please enter email & password", 400));
   }
 
-  const user = await UserModel.findOne({ email }).select("+password"); //.select("+password") because in schema we set set select : false so password is'nt return to anyone so we add +password here for verfication of pass
+  const user = await UserModel.findOne({ email }).select("+password");
 
-  if (user) {
-    const isPasswordCorrect = await user.comparePassword(password);
-
-    if (isPasswordCorrect) {
-      sendJwtToekn(user, 201, res);
-    } else {
-      return next(new ErrorHandler("Invalid email or password", 401));
-    }
-  } else {
+  if (!user) {
     return next(new ErrorHandler("Invalid email or password", 401));
   }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendJwtToekn(user, 200, res);
 });
 
+// @desc    Logout user
+// @route   GET /api/user/logout
+// @access  Private
+exports.logoutUser = asyncWrapper(async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-exports.logoutUser = asyncWrapper(async (req , res) =>{
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
+});
 
-   
+// @desc    Load current user
+// @route   GET /api/user/me
+// @access  Private
+exports.loadUser = asyncWrapper(async (req, res, next) => {
+  const user = await UserModel.findById(req.user._id);
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
 
-   const options = {
-     expires: new Date(
-       Date.now()
-     ), 
-     httpOnly: true,
-   };
-
-   res.status(200).cookie("token", null, options).json({
-     success: true,
-     message : "Loged out successfully"
-   });
-
-
-})
-
-
-
-exports.loadUser = asyncWrapper (async( req , res) =>{
-
-
-  const user = await  userModel.findById(req.user._id);
-
-   res.send(user);
-})
+  res.status(200).json(user);
+});
