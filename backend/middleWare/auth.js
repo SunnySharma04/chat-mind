5de32faa3c941473f5da5ad7c1718b61ce0d83b6
SunnySharma04 <sunnysharma112004@gmail.com>
2025-08-03 20:49:09 +0530

    Update auth.js

--- backend/middleWare/auth.js
+++ backend/middleWare/auth.js
@@ -1,20 +1,38 @@
-const asyncWrapper  = require("../middleWare/asyncWrapper");
+const asyncWrapper = require("../middleWare/asyncWrapper");
 const jwt = require("jsonwebtoken");
-const ErrorHandler  = require("../appUtills/error");
+const ErrorHandler = require("../appUtills/error");
 const userModel = require("../model/UserModel");
 
+exports.authentication = asyncWrapper(async (req, res, next) => {
+  let token;
 
-exports.authentication = asyncWrapper( async (req , res , next) =>{
-const token = req.cookies.token;
-if (!token) {
-  return next(new ErrorHandler("Please Login to access this resource", 401));
-}
+  // 1. Check Authorization header: Bearer <token>
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+  // 2. If not in header, try cookie
+  if (!token && req.cookies?.token) {
+    token = req.cookies.token;
+  }
 
+  if (!token) {
+    return next(new ErrorHandler("Please login to access this resource", 401));
+  }
 
-  // get user data  & store as {req.user} using user_Id from decodeToken added as payload while token created
- req.user = await userModel.findById(decodeToken.id);
-  next();
-   
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await userModel.findById(decoded.id).select("-password"); // optional: exclude password
+    if (!req.user) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+
+    next();
+  } catch (err) {
+    return next(new ErrorHandler("Invalid token, login again", 401));
+  }
 });
